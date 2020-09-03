9f9b0dc3e5c21bcadc4055a257a39e35606935a3
pankajChetry <pankajchetry22@gmail.com>
2020-09-03 22:21:38 +0530

    added codes in views,app.component.html and ts

--- Upload_file_app/App_fileupload/views.py
+++ Upload_file_app/App_fileupload/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from django.http import HttpResponse
 from .serializers import FileSerializer
 from .models import file
--- fileUpload-app/src/app/app.component.ts
+++ fileUpload-app/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fileUpload-app';
+  title :string;
+  upload_file:File;
+
+  onTitleChanged(event:any) {
+    this.title = event.target.value;
+  }
+
+  onFileChanged(event:any) {
+    this.upload_file = event.target.files[0];
+  }
+
 }
